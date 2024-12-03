178d064dd4e88df59713951b5a6c4acaab63bf89
Steve Hunneyball <stevehunneyball@gmail.com>
2024-12-03 10:47:20 +0000

    refactor and tidy

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
 import 'dotenv/config';
 import {Server} from "./server";
+import {subscribe} from "./subscriber";
 
 async function entry() {
-    const supabase = createClient(
-        process.env.SUPABASE_URL ?? "",
-        process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
-    );
-
     console.log("Establishing NATS connection...");
     const server = new Server()
     await server.establishConnection()
-
-    console.log('Subscribing to changes...');
-
-    supabase
-        .channel('schema-db-changes')
-        .on(
-            'postgres_changes',
-            {
-                event: 'INSERT',
-                schema: 'public'
-            },
-            (payload: string) => {
-                console.log('Insert received!', payload)
-                server.publishMessage("potato.masher", payload)
-            }
-        )
-        .on(
-            'postgres_changes',
-            {
-                event: 'UPDATE',
-                schema: 'public'
-            },
-            (payload: string) => {
-                console.log('Update received!', payload);
-                server.publishMessage("hello.world", payload)
-            }
-        )
-        .subscribe();
-
-    console.log("Subscribed.");
+    await subscribe(server).then(() => console.log("Subscribed to DB changes."));
 }
 
-entry().then(() => console.log("Process established."));
+entry().then(() => console.log("Process established. Awaiting changes."));
---
+++ src/subscriber.ts
@@ -0,0 +1,37 @@
+import {createClient} from "@supabase/supabase-js";
+import {Server} from "./server";
+
+export async function subscribe(server: Server) {
+    const supabase = createClient(
+        process.env.SUPABASE_URL ?? "",
+        process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
+    );
+
+    supabase
+        .channel('schema-db-changes')
+        .on(
+            // @ts-ignore
+            'postgres_changes',
+            {
+                event: 'INSERT',
+                schema: 'public'
+            },
+            (payload: string) => {
+                console.log('Insert received!', payload)
+                server.publishMessage("potato.masher", payload)
+            }
+        )
+        .on(
+            // @ts-ignore
+            'postgres_changes',
+            {
+                event: 'UPDATE',
+                schema: 'public'
+            },
+            (payload: string) => {
+                console.log('Update received!', payload);
+                server.publishMessage("hello.world", payload)
+            }
+        )
+        .subscribe();
+}
